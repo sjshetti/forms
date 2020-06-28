6c00e8f2075ee87616bc002983750fc689929a1b
sjshetti <61972410+sjshetti@users.noreply.github.com>
2020-06-28 11:36:54 +0530

    a page with a form

---
+++ NSS_Message.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="Styles.css" type="text/css">
+    </head>
+    <body>
+        <div class="form">
+        Hi <?php echo $_POST["name"];?>, Your Roll number (<?php echo $_POST["rollno"];?>) has been successfully recorded!
+            </div>
+    </body>
+</html>
---
+++ NSS_problem2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <form action="NSS_Message.php" method="post">
+            Name: <input type="text" name="name" placeholder="Your Name">
+            Roll No.: <input type="text" name="rollno" placeholder="Your Roll No.">
+            <input type="submit">
+        </form>
+    </body>
+</html>
